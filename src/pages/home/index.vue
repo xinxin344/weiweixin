826c664c290362f8dbf51f9e7f1d40dabdd530ac
黄涛 <huangtao@huangtaodeMacBook-Pro.local>
2023-03-21 17:10:02 +0800

    feat: 添加vant

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="">
-    首页
+    <van-button type="primary">首页</van-button>
+    <van-search v-model="value" placeholder="请输入搜索关键词" />
     <div>111</div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from 'vue'
+
+let value = ref('')
+
+watch(
+  value,
+  (cur) => {
+    console.log(cur);
+  }
+)
+
 
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import WindiCSS from 'vite-plugin-windicss'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     WindiCSS(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
